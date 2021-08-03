ae16e704d7034c75ede44afcd555d877b12127a1
wnshrla <wnshrla@naver.com>
2021-08-03 15:38:03 +0900

    인터페이스 구현

---
+++ core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,13 @@
+package hello.core.member;
+
+public class MemoryMemberRepository implements MemberRepository {
+    @Override
+    public void save(Member member) {
+        
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return null;
+    }
+}

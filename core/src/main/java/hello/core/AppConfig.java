b4e0e6fad4ccd88d8ffffac2bf2c33a3439d44ee
wnshrla <wnshrla@naver.com>
2021-08-04 16:43:40 +0900

    no message

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,4 +1,13 @@
 package hello.core;
 
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+
 public class AppConfig {
+
+    // 생성자 주입
+    public MemberService memberService() {
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
 }

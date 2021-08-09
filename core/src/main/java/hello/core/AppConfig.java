d36a9bbfe5446f2782171e9cf1a52f0dd8b071e6
wnshrla <wnshrla@naver.com>
2021-08-09 17:14:24 +0900

    싱글톤 컨테이너

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -18,16 +18,19 @@ public class AppConfig {
     // 생성자 주입
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
